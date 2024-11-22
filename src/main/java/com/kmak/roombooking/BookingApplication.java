35c6988d1445f1030fff7133a9570c7306505fb9
kitmak <kitmak.72@gmail.com>
2024-11-22 12:26:50 +0800

    rename package

--- src/main/java/com/kmak/roombooking/BookingApplication.java
+++ src/main/java/com/kmak/roombooking/BookingApplication.java
@@ -1,4 +1,4 @@
-package com.kmak.booking;
+package com.kmak.roombooking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/kmak/roombooking/BookingApplicationTests.java
+++ src/test/java/com/kmak/roombooking/BookingApplicationTests.java
@@ -1,4 +1,4 @@
-package com.kmak.booking;
+package com.kmak.roombooking;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
